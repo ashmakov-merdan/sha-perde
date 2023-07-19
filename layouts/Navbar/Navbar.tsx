517543afcbca72dbef7c5e61ba6db38beb6ffe37
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-19 12:32:12 +0500

    Navbar

--- layouts/Navbar/Navbar.tsx
+++ layouts/Navbar/Navbar.tsx
@@ -1,16 +1,21 @@
-import { Languages, NavList } from "@/components";
+"use client"
+import { Burger, Languages, NavList } from "@/components";
 import Link from "next/link"
 import { Logo } from "@/assets/icons";
 import "@/styles/navbar.scss";
+import useToggle from "@/hooks/useToggle";
 
 const Navbar = () => {
+  const { open, toggle } = useToggle();
+
   return (
     <nav id="navbar" className="container">
       <Link id="logo" href="/">
           <Logo />
       </Link>
-      <NavList />
+      <NavList open={open} toggle={toggle} />
       <Languages  />
+      <Burger open={open} toggle={toggle} />
     </nav>
   )
 };
