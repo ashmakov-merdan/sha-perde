c4a0e132d08f1f9ed62aa03eb70c629e1e6d0617
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-30 21:41:58 +0500

    About section

---
+++ layouts/Home/About/About.tsx
@@ -0,0 +1,33 @@
+"use client"
+import "@/styles/about.scss";
+import Image from "next/image";
+import pattern from "@/assets/images/pattern.svg";
+import about from "@/assets/images/about.png";
+import { Button } from "@/components";
+
+const About = () => {
+  return (
+    <section id="about">
+      <span id="pattern">
+        <Image src={pattern} width={300} height={300} alt="pattern"/>
+      </span>
+      <div className="container">
+          <div className="row">
+            <p className="heading">Кто мы?</p>
+            <h1 className="whoweare">Мы работаем напрямую с ведущими европейскими производителями в сфере декорирования помещений.</h1>
+          </div>
+          <div className="row">
+            <div className="col">
+              <p>Компания Şa Perde была создана более 10 лет тому назад и на сегодняшний день является одним из самых крупных поставщиков штор, карнизов на окна, жалюзей, занавесей и аксессуаров домашнего интерьера в Туркменистане. Мы работаем напрямую с ведущими европейскими производителями в сфере декорирования помещений.</p>
+              <Button link={"/about"}>Узнат болше о нас</Button>
+            </div>
+            <div className="col">
+              <Image src={about} alt={"about"} />
+            </div>
+          </div>
+      </div>
+    </section>
+  )
+};
+
+export default About;
