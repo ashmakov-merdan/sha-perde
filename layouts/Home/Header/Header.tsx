f592726576238511a3fd951e549f918f78878718
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-30 21:42:28 +0500

    Header section

---
+++ layouts/Home/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { Carousel, Heading, Button } from "@/components";
+import "@/styles/header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <Heading>owadanlyk siziň öýüňizde!</Heading>
+          <p className="description">
+            На сегодняшний день компания Şa Perde  является одним из самых крупных поставщиков штор, карнизов на окна, жалюзей и занавесей в Туркменистане.
+          </p>
+          <Button link={"/about"}>Узнат болше о нас</Button>
+        </div>
+      </div>
+      <Carousel />
+    </header>
+  )
+};
+
+export default Header;
