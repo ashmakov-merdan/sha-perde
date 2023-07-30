cb550414a5cf5b618423e1109348149a3cf476f9
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-30 21:38:21 +0500

    Component exports

--- components/index.tsx
+++ components/index.tsx
@@ -3,5 +3,8 @@ import NavList from "./Navbar/NavList";
 import Carousel from "./Carousel/Carousel";
 import Heading from "./Heading/Heading";
 import Button from "./Button/Button";
+import Burger from "./Burger/Burger";
+import Service from "./Service/Service";
+import Map from "./Map/Map";
 
-export { Languages, NavList, Carousel, Heading, Button };
+export { Languages, NavList, Carousel, Heading, Button, Burger, Service, Map };
