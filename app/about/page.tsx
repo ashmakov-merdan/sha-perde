ba5652347872ed2c97d986a9ac68c1004cb9d7c6
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-30 21:39:14 +0500

    About page

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "@/layouts/About";
+import { Header, Content } from "@/layouts/About";
+import "@/styles/about/styles.scss";
+
+const About = () => {
+  return (
+    <>
+    <Header />
+    <main>
+      <Content />
+    </main>
+    <Footer />
+    </>
+  )
+}
+
+export default About;
