82804c90fe8b2c4c67aad93bf7cdeec932712841
Merdan Ashmakov <ashmakov.m@gmail.com>
2023-07-30 21:36:32 +0500

    Button Component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,11 @@
-import { ComponentProps } from "@/types";
+"use client"
+import { ButtonProps } from "@/types";
+import { useRouter } from "next/navigation";
 
-const Button = ({children}: ComponentProps) => {
-  return <button id="button">{children}</button>
+const Button = ({children, link}: ButtonProps) => {
+  const router = useRouter();
+
+  return <button id="button" onClick={() => router.push(link)}>{children}</button>
 };
 
 export default Button;
